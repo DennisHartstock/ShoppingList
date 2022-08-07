2b904b550ff480daac0495b40daf4ff796500cdc
DennsHartstock <dennis.hartstock@web.de>
2022-08-07 09:08:16 +0200

    add DataBase

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,21 @@
+package com.example.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<ShopItemDbModel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addSopItem(shopItemDbModel: ShopItemDbModel)
+
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItem(shopItemId: Int)
+
+    @Query("SELECT  * FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId: Int): ShopItemDbModel
+}
